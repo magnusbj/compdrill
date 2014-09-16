8229578e5e3965e62c34276930fee6f4616ec7d2
magnusbj <magnusbj@gmail.com>
2014-09-16 20:26:07 +0000

    unable to use index.html from /views, tried copying from bitstarter on courseragit. Need to copy the ./models from courseragit

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // init module usage
 var express = require('express')
+  , http = require('http')
+  , https = require('https')
+  , fs = require('fs')
+  , db = require('./models');
 
 // init db and app
 var app = express()
@@ -7,6 +11,8 @@ var app = express()
 
 // init views
 app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs');
+app.set('port',process.env.PORT || 8080);
 
 
 app.configure('development', function(){
@@ -18,9 +24,20 @@ app.configure('development', function(){
 app.use(express.static(__dirname + '/public'))
 
 // render pages
- app.get('/', function(req, res){
- res.render('index', {title: 'hello world'})
-})
+app.get('/', function(req, res){
+  var data = fs.readFileSync('index.html').toString();
+  response.send(data);
+// res.render('index', {title: 'hello world'}) 
+});
 
 // listen on port
-app.listen(8080)
+db.sequelize.sync().complete(function(err) {
+  if (err) {
+      throw err;
+  } else {
+    http.createServer(app).listen(app.get('port'), function() {
+      console.log("Listening on " + app.get('port'));
+    });
+  }
+});
+// app.listen(8080)

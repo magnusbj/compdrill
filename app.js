fc3f6de5776a10554c422bcd73b1471ee8e3a7fa
magnusbj <magnusbj@gmail.com>
2014-09-17 11:04:20 +0000

    Not able to access /views/ folder

--- app.js
+++ app.js
@@ -1,43 +1,23 @@
-// init module usage
-var express = require('express')
-  , http = require('http')
-  , https = require('https')
-  , fs = require('fs')
-  , db = require('./models');
+// attempt to write my own app
+// use html files in the /views folder
+// css script and html files from bootstrap
+// run with express at a minum, with ejs as view engine
 
-// init db and app
-var app = express()
+var express = require('express');
+var fs = require('fs');
+var http = require('http');
 
-
-// init views
-app.set('views', __dirname + '/views')
+var app = express();
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.set('port',process.env.PORT || 8080);
+app.set('port', process.env.PORT || 8080);
 
-
-app.configure('development', function(){
- app.use(express.errorHandler())
- app.locals.pretty = true
-})
-
-// init static public directory
-app.use(express.static(__dirname + '/public'))
-
-// render pages
-app.get('/', function(req, res){
+app.get('/', function(request, response){
   var data = fs.readFileSync('index.html').toString();
   response.send(data);
-// res.render('index', {title: 'hello world'}) 
 });
 
-// listen on port
-db.sequelize.sync().complete(function(err) {
-  if (err) {
-      throw err;
-  } else {
-    http.createServer(app).listen(app.get('port'), function() {
-      console.log("Listening on " + app.get('port'));
-    });
-  }
+// start the app
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Listening on " + app.get('port'));
 });
-// app.listen(8080)

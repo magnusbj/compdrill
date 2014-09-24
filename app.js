3eb7a282be94efa8f68dde4e181adfb8236ddd23
magnusbj <magnusbj@gmail.com>
2014-09-24 08:08:08 +0000

    With cover page, learning to communicate

--- app.js
+++ app.js
@@ -9,9 +9,10 @@ var json = require('express-json');
 var app = express();
 var loadPosts = express.Router();
 
-//app.set('views', __dirname + '/views/')
-app.set('views', __dirname + '/jumbo/')
-app.use(express.static(__dirname + '/jumbo/'));
+app.set('views', __dirname + '/views')
+//app.set('views', __dirname + '/jumbo/')
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 8080);
@@ -20,42 +21,55 @@ app.set('port', process.env.PORT || 8080);
 app.locals.title= 'Progressive Drilling and Well solutions';
 app.use(json());
 
-app.use('/', loadPosts);
+app.use('/posts', loadPosts);
 
 // function skrivNoeTull(tull) {}
 // var skrivNoeTull = function(tull) {console.log(tull);}
 // skivNoeTull("tull");
 
 loadPosts.use(function(req, res, next){
-    fs.readFile('./texts/slogan.txt', function (err, slogandata){
-	var slogan = slogandata;
-	res.slogan = slogan;
-	console.log("slogan loaded to req.slogan");
-	});
-    fs.readFile('./texts/stories.json', function(err, data){
-	var posts = data;
-	var parsed = JSON.parse(posts);
-	var title =[];
-	var slug = [];
-	var content = [];
-	var i = 0;
-	parsed.forEach(function(post) {
-	    title[i] = post.title;
-	    slug[i] = post.slug;
-	    content[i] = post.content;
-	    i++;
-	});
-	console.log("Posts loaded into title, slug and content");
-	res.render('index.ejs', {headline: "Newface", content1: "example. need to load file", postlength: title.length, posttitle: title, postslug: slug, postcontent: content, slogan: res.slogan});
+    fs.readFile('public/texts/slogan.txt', function (err, slogandata){
+	if (err){console.log("didn't read slogandata");}
+	else{
+	    var slogan = slogandata;
+	    res.slogan = slogan;
+	    console.log("slogan loaded to req.slogan");
+	}
+    });
+    fs.readFile('public/texts/stories.json', function(err, data){
+	if (err){console.log("didn't read stories");}
+	else{
+	    var posts = data;
+	    var parsed = JSON.parse(posts);
+	    var title =[];
+	    var slug = [];
+	    var content = [];
+	    var i = 0;
+	    parsed.forEach(function(post) {
+		title[i] = post.title;
+		slug[i] = post.slug;
+		content[i] = post.content;
+		i++;
+	    });
+	    console.log("Posts loaded into title, slug and content");
+	    res.render('jumbotron.ejs', {headline: "Composite Drilling", postlength: title.length, posttitle: title, postslug: slug, postcontent: content, slogan: res.slogan});
+	}
+	next();
+    });
 
-    next();
-  });
 });
 
 app.get('/empty', function(request, response){
     fs.readFile('./texts/slogan.txt', function (err, data){
     response.render('empty.ejs', { headline: 'Composite Drilling'});
     });
+});
+
+app.get('/', function(req, res){
+ res.render('cover.ejs', {heading: "Welcome"});
+
+//    res.render('cover.ejs', {title:'Smarter drilling', heading: 'Say welcome to faster and safer operations'});
+    console.log('Rendered cover page');
 });
 
 app.get('/post/:slug', function(req,res, next){
